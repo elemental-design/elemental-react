300392b62793a92aba3b79254afb02594b3c6c89
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-03-09 21:50:00 +0000

    setup typescript build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,22 @@
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 // import alias from 'rollup-plugin-alias';
 
 import packageJson from './package.json';
+
+const getPlugins = (platform) => [
+  json(),
+  babel({
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.es6', '.es', '.mjs'],
+    exclude: 'node_modules/**',
+  }),
+  resolve({
+    extensions: ['.mjs', `.${platform}.js`, `.${platform}.jsx`, `.${platform}.ts`, `.${platform}.tsx`, '.js', '.ts', '.jsx', '.tsx', '.json'],
+  }),
+  commonjs(),
+];
 
 const output = {
   // file: 'dist/bundle.esm.js',
@@ -16,30 +28,30 @@ const output = {
     'react-dom': 'ReactDOM',
     'styled-components': 'styled',
   },
+  // plugins: getPlugins('web'),
 };
 
-export default {
-  input: 'src/index.js',
-  output: [
-    output, {
-      ...output,
-      file: './dist/sketchBundle.js',
-    }],
-  plugins: [
-    json(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    // alias({
-    //   resolve: ['.jsx', '.js'],
-    //   entries: [
-    //     { find: '~', replacement: './src/' },
-    //   ],
-    // }),
-    resolve({
-      extensions: ['.mjs', '.sketch.js', '.js', '.jsx', '.json'],
-    }),
-    commonjs(),
-  ],
-  external: ['react', 'react-native-web', 'react-dom', 'react-is', 'styled-components', 'styled-system', 'react-primitives', 'react-primitives-svg'],
-};
+export default [
+  // Web export
+  {
+    input: 'src/index.ts',
+    output: [
+      output
+    ],
+    external: ['react', 'react-native-web', 'react-dom', 'react-is', 'styled-components', 'styled-system', 'react-primitives', 'react-primitives-svg'],
+    plugins: getPlugins('web')
+  },
+  // Sketch export
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        ...output,
+        file: './dist/sketchBundle.js',
+        // plugins: getPlugins('sketch'),
+      }
+    ],
+    external: ['react', 'react-native-web', 'react-dom', 'react-is', 'styled-components', 'styled-system', 'react-primitives', 'react-primitives-svg'],
+    plugins: getPlugins('sketch')
+  },
+]
